de9aebeba8f571e05cce19971e8ea830b4835ecc
Ikebata <ikebata@lean-system.co.jp>
2019-04-01 14:59:30 +0900

    Add android provisioner

---
+++ vagrant_scripts/android.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+__DIR__=$(cd $(dirname $0); pwd)
+ANDROID_PATH=${SCRIPTS_PATH:-$__DIR__/../model-town/build/android}
+DEPS_PATH=$ANDROID_PATH/deps
+
+echo Making android...
+cd $ANDROID_PATH
+make
+
+echo Patching os.cpp...
+cd $DEPS_PATH
+patch -p1 < ../patches/os.patch
+
+echo Making android...
+cd ../
+make
+
+echo Patching sqlite3.c...
+cd $DEPS_PATH
+patch -p1 < ../patches/sqlite3.patch
+
+echo Making android...
+cd ../
+make
