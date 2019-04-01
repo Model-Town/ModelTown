dc582829146e8ff1fe80561da8c291b4d15fdf40
FUJISE Takumi <fujise@lean-system.co.jp>
2019-04-01 14:44:09 +0900

    use /tmp

--- vagrant_scripts/install_gradle.sh
+++ vagrant_scripts/install_gradle.sh
@@ -4,9 +4,9 @@ INSTALL_PATH=$HOME/android
 
 echo Install gradle...
 mkdir -p $INSTALL_PATH
-cd $INSTALL_PATH
-wget https://services.gradle.org/distributions/gradle-2.13-all.zip
-unzip gradle-2.13-all.zip
+wget https://services.gradle.org/distributions/gradle-2.13-all.zip -P /tmp
+unzip -d $INSTALL_PATH /tmp/gradle-2.13-all.zip
+rm -rf /tmp/gradle-2.13-all.zip
 
 if [ -z "$(grep 'gradle-2.13' $HOME/.bashrc)" ] ; then
   echo 'export PATH=$PATH:'$INSTALL_PATH'/gradle-2.13/bin' >> $HOME/.bashrc
