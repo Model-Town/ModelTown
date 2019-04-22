728a97f4db38efeb2f148c8e3bb59bad7d0aca14
Ikebata <ikebata@lean-system.co.jp>
2019-04-22 17:27:44 +0900

    Add packages for  building desktop platform

--- vagrant_scripts/install_packages.sh
+++ vagrant_scripts/install_packages.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
 
 echo Install packages...
+# for building android source code
 sudo apt-get install -y make m4 subversion git-core build-essential realpath openjdk-8-jdk libncurses5 lib32z1 unzip
+# for building desktop platform source code
+sudo apt-get install -y build-essential cmake git libirrlicht-dev libbz2-dev libgettextpo-dev libfreetype6-dev libjpeg8-dev libxxf86vm-dev libgl1-mesa-dev libsqlite3-dev libogg-dev libvorbis-dev libopenal-dev libhiredis-dev libcurl3-dev
